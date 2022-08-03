c93c6819ac57f27062b7f901859088c50978c3c1
iliyasupermonstr <iliya.bezrukov@icloud.com>
2022-08-03 13:26:35 +0400

    upd

--- main.py
+++ main.py
@@ -1,14 +1,1 @@
-import turtle
-t = turtle.Pen() #создаем ручку для черепашки
-
-
-for i in range (4):
-    t.forward(100)
-    t.right(90)
-
-
-
-
-
-
-input("Для продолжения нажмите любую клавишу...")
+print ("Ваня лох")
